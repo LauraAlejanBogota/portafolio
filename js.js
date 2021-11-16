58b34822f3ab166dc8abc938c027cc8bdac76720
Laura bogota <lauralejab96@gmail.com>
2021-11-16 11:28:17 -0500

    actualizacion portafolio

---
+++ js.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+// laura//
+document.addEventListener("submit",(e)=>{
+    //e.preventDefault();
+    alert("Enviando formulario");
+});
